18df5a9d74b8e2e704a80edb85a41c9e7faae0d3
75u2u <75u2uuy4@gmail.com>
2017-08-01 19:56:26 +0900

    update

--- Binary.cpp
+++ Binary.cpp
@@ -2,17 +2,17 @@
 
 using namespace cv;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+
 	Mat img = imread("input.jpg");
 	Mat gray_img;
 	Mat element = Mat::ones(3, 3, CV_8UC1); //3×3の行列で要素はすべて1
 	cvtColor(img, gray_img, CV_BGR2GRAY);	//グレースケール化
 
 	Mat bin_img;
-	threshold(gray_img, bin_img, 127, 255, THRESH_BINARY); //閾値設定
+	threshold(gray_img, bin_img, 127, 255, THRESH_BINARY);	//閾値設定
 	//bin_img = ~bin_img; //反転
-	dilate(bin_img, bin_img, element, Point(-1, -1), 0); //最後の引数で膨張処理回数を設定
+	dilate(bin_img, bin_img, element, Point(-1, -1), 0);	//最後の引数で膨張処理回数を設定
 
 	imshow("result", bin_img);
 
--- GammaCorrection.cpp
+++ GammaCorrection.cpp
@@ -2,10 +2,9 @@
 
 using namespace cv;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	// 画像の取得
-	Mat im = imread("input.jpg");
+	Mat img = imread("input.jpg");
 	// 出力画像
 	Mat result;
 
@@ -15,14 +14,14 @@ int main(int argc, char* argv[])
 	//　ルックアップテーブル作成
 	uchar lut[256];
 	double gm = 1.0 / gamma;
-	for (int i = 0; i < 256; i++)
+	for(int i = 0; i < 256; i++)
 		lut[i] = pow(1.0*i / 255, gm) * 255;
 	
 	// 輝度値の置き換え処理
-	 LUT(im, Mat(Size(256, 1), CV_8U, lut), result);
+	 LUT(img, Mat(Size(256, 1), CV_8U, lut), result);
 
 	// 元画像の表示
-	imshow("original", im);
+	imshow("original", img);
 	// 結果画像の表示
 	imshow("result", result);
 
